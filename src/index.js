686938347e9cf3a636c8463f5c997c2db671c935
Brian James <brianpja@gmail.com>
2018-01-30 14:00:34 -0800

    new components

--- src/index.js
+++ src/index.js
@@ -1,9 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+import SearchBar from './components/search_bar';
+
+const API_KEY = 'AIzaSyCzcy_rEt8HvpFtP-uBHjnExyFHLkW-Yow';
+
 // Create new component with some html
 const App = () => {
-  return <div>Brian, you handsome!</div>;
+  return (
+    <div>
+      <div>Brian, you handsome!</div>
+      <SearchBar />
+    </div>
+  );
 }
 
 // put that html on the page (DOM)
