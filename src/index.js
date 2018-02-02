960cefd9f32324985bbd528eba1ae184633e2580
Brian James <brianpja@gmail.com>
2018-02-02 11:34:24 -0800

    search bar functionality

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ import YTSearch from 'youtube-api-search';
 
 import SearchBar from './components/search_bar';
 import VideoList from './components/video_list';
+import VideoDetail from './components/video_detail';
 
 const API_KEY = 'AIzaSyCzcy_rEt8HvpFtP-uBHjnExyFHLkW-Yow';
 
@@ -14,22 +15,32 @@ class App extends Component {
   constructor(props) {
     super(props);
 
-    this.state = { videos: [] };
+    this.state = {
+      videos: [],
+      selectedVideo: null
+    };
 
-    YTSearch({key: API_KEY, term: 'surfboards'}, (videos) => {
-      this.setState({ videos });
-      // this.setState({ videos: videos });
+    this.videoSearch('cats')
+  }
+
+  videoSearch(term) {
+    YTSearch({key: API_KEY, term: term}, (videos) => {
+      this.setState({
+        videos: videos,
+        selectedVideo: videos[0]
+      });
     })
-
-
   }
 
   render() {
     return (
       <div>
         <div>Brian, you handsome!</div>
-        <SearchBar />
-        <VideoList videos={this.state.videos} />
+        <SearchBar onSearchTermChange={term => this.videoSearch(term)}/>
+        <VideoDetail video={this.state.selectedVideo}/>
+        <VideoList
+          onVideoSelect={ selectedVideo => this.setState({selectedVideo}) }
+          videos={this.state.videos} />
       </div>
     );
   }
